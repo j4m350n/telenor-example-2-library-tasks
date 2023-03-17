5e24dabdf9e9166571821d4f395828ea512249e8
Jameson <j4m350n@testersen.no>
2023-03-18 00:50:46 +0100

    Renamed returnValue to value in TaskResult

--- src/main/java/no/jamesb/task/TaskResult.java
+++ src/main/java/no/jamesb/task/TaskResult.java
@@ -11,14 +11,14 @@ public class TaskResult<T> {
 
 	public final boolean didThrow;
 	public final Exception exception;
-	public final T returnValue;
+	public final T value;
 
 	public TaskResult(boolean didThrow, Exception exception, T value) {
 		this.didThrow = didThrow;
 		this.exception = exception;
-		this.returnValue = value;
+		this.value = value;
 
-		if (!this.didThrow && this.returnValue == null) {
+		if (!this.didThrow && this.value == null) {
 			throw new NullPointerException("The provided return value cannot be null when no exception was thrown!");
 		} else if (this.didThrow && this.exception == null) {
 			throw new NullPointerException("The exception cannot be null when an error was expected!");
